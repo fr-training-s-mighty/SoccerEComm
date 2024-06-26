949ed5a4067d1ec70a687243e80504e32f9d1b84
Shivaansh Mital <fr.shivaansh.mital@prolim.in>
2024-06-26 16:58:17 +0000

    Adding

--- SportyShoe/src/main/java/com/sports/major/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sports.major.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sports.major.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-
-}
